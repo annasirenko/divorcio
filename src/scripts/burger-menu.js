b0d224794999eb672e8efd574a33840ed241f74b
Anna <otherjam@gmail.com>
2021-01-15 15:42:22 +0200

    add adaptive layout part

---
+++ src/scripts/burger-menu.js
@@ -0,0 +1,24 @@
+//burger nav
+
+const nav = document.querySelector(".nav__list");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+const close = document.querySelector(".close-btn");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav__list--open"); 
+  close.classList.toggle("close-btn--close");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav__list--open");    
+    close.classList.toggle("close-btn--close");
+  });
+});
+
+close.addEventListener("click", () => {
+    nav.classList.toggle("nav__list--open");   
+    close.classList.toggle("close-btn--close");
+  });
+
